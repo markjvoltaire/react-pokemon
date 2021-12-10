2019c6311e3b83c9c874b2ba183575b7d295a17b
Mark Voltaire <markjvoltaire@gmail.com>
2021-12-09 21:29:08 -0500

    images do not show up

--- src/services/Componets/pokemon.js
+++ src/services/Componets/pokemon.js
@@ -1,9 +1,16 @@
 export async function getPokemon(query) {
   const params = new URLSearchParams();
   params.set('pokemon', query);
+
   const response = await fetch(
     `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
   );
   const data = await response.json();
   return data;
+}
+
+export async function getType() {
+  const response = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex/types');
+  const data = await response.json();
+  return data.map((item) => item.type);
 }
--- src/services/Componets/pokemonlist/Pokemonlist.js
+++ src/services/Componets/pokemonlist/Pokemonlist.js
@@ -4,7 +4,11 @@ export default function Pokemonlist({ pokemon }) {
   return (
     <div>
       {pokemon.map((poke) => (
-        <p key={poke.id}>{poke.pokemon}</p>
+        <p key={poke.id}>
+          {poke.pokemon}
+          <img src={poke.url_image}></img>
+          <span>Type: {poke.type_1}</span>
+        </p>
       ))}
     </div>
   );
